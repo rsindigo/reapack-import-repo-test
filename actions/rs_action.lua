d729665e50b337b8ab1771a978998adb73ee5086
Your Name <your_email@example.com>
2026-07-11 19:56:39 -0500

    first commit

---
+++ actions/rs_action.lua
@@ -0,0 +1,10 @@
+--[[
+    @description import repo test - rs action test
+    @author rs indigo
+    @about
+        testing how the import scheme works...
+    @version 1.0
+
+]]--
+
+reaper.ShowConsoleMsg("it works")
